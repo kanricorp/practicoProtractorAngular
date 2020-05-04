262e2a38d46c2a8138972e504f1c0db25455ade1
kanricorp <57548352+kanricorp@users.noreply.github.com>
2020-05-04 11:33:12 -0300

    Update testing_page.js

--- pages/testing_page.js
+++ pages/testing_page.js
@@ -1,9 +1,14 @@
 var testingPage = function() {
     var EC = require("protractor").ExpectedConditions;
 
-    this.validarPagina = function(string) {
+    this.validarPagina = function() {
         browser.wait(EC.elementToBeClickable($('.lightbox')),5000);
-        return browser.wait(EC.visibilityOf($('#testing')),5000);
+        browser.wait(EC.visibilityOf($('#testing')),5000);
+        return element(By.id('testing')).getText();
+    }
+
+    this.validarURL = function(string) {
+        return browser.wait(EC.urlContains(string), 5000);
     }
 
 };
